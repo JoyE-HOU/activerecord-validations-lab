6da3f35c3f9097648bdd95c858db086a92dbf8a2
Joy Evans <joyevans@me.com>
2020-12-22 10:06:02 -0600

    commit tree aee3cff4b03a15d97cea922207aa66d840e8b76b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { :in => %w(Fiction Non-Fiction)}
+    validates :invalid_if_not_clickbait, inclusion: { :in => %w("Won't Believe" "Secret" "Top [number]" "Guess")}
+    # validates :invalid_if_not_clickbait{("Won't Believe", "Secret", "Top [number]", "Guess")}
+
+    # private
+
+    def invalid_if_not_clickbait
+        if title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
+            puts "true"
+        else
+            puts "false"
+        end
+    end
 end
